a05309d16c5d42bc167ef9a2ccbaf43a9ccd465d
kefirim <rim.kefidev@gmail.com>
2023-12-07 11:04:57 +0100

    add project

---
+++ routes/root.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+
+//console.log(path.join(__dirname, '..'))
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+module.exports = router;
