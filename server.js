7c35660fd97c4e9a28ac45e69ebdac8adc1b2340
kefirim <rim.kefidev@gmail.com>
2023-12-07 11:11:34 +0100

    add

--- server.js
+++ server.js
@@ -21,18 +21,18 @@ app.use("/", require("./routes/root"));
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/users", require("./routes/userRoutes"));
 
-app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-      res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-      res.json({ message: "404 Not Found" });
-    } else {
-      res.type("txt").send("404 Not Found");
-    }
-  });
+//app.all("*", (req, res) => {
+  //  res.status(404);
+ //   if (req.accepts("html")) {
+  //    res.sendFile(path.join(__dirname, "views", "404.html"));
+  //  } else if (req.accepts("json")) {
+   //   res.json({ message: "404 Not Found" });
+   // } else {
+   //   res.type("txt").send("404 Not Found");
+  //  }
+ // });
 
-//*************connected database************************************** */
+//*************connected database************************************* */
 connectDB();
   mongoose.connection.once("open", () => {
  console.log("Connected to MongoDB");
